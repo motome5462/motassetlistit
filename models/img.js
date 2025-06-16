a3b6a4153523f55cc6415936b25bfe6e0010942a
palm9086 <118614294+palm9086@users.noreply.github.com>
2025-06-16 16:52:05 +0700

    Delete img.js

--- models/img.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const imgSchema = new mongoose.Schema({
-    img: { type: String, default: "" },
-    assetid: { type: mongoose.Schema.Types.ObjectId, ref: "assetlist" },
-});
-
-module.exports = mongoose.model('img', imgSchema);
